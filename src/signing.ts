1ebba4b81683efbf7036953d10561946cbeebe48
Rupert Dunk <rupert@rupertdunk.com>
2025-09-18 15:31:15 +0100

    fix: do not encode signature (#17)
    It turns out the signature itself is currently being encoded by `URLSearchParams`, which obviously causes a mismatch on the server when validating.
    
    This PR switches to appending the signature using string concatenation instead to avoid encoding issues.

--- src/signing.ts
+++ src/signing.ts
@@ -37,21 +37,42 @@ export function generateSignature(url: string | URL, privateKey: string): string
  */
 export function signUrl(url: string | URL, options: SigningOptions): string {
   const {expiry, keyId, privateKey} = options
-  const baseUrl = new URL(url)
+  // Get the URL with signing parameters (keyid and expiry)
+  const urlWithParams = urlWithSigningParams(url, {expiry, keyId})
+  // Generate a signature from the full URL with the new parameters
+  const signature = generateSignature(urlWithParams, privateKey)
+  // Append the signature using string concatenation instead of URLSearchParams
+  // to ensure the signature is not URL-encoded (e.g. '=' to '%3D')
+  const urlWithSignature = `${urlWithParams.toString()}&signature=${signature}`
+  return urlWithSignature
+}
+
+/**
+ * Returns a ready-to-sign URL object with signing parameters (keyid and optional expiry).
+ *
+ * @public
+ * @param url - The base URL to which signing parameters will be added
+ * @param options - The signing options containing keyId and optional expiry
+ * @returns A URL object with keyid and expiry parameters added
+ */
+export function urlWithSigningParams(
+  url: string | URL,
+  options: Omit<SigningOptions, 'privateKey'>,
+): URL {
+  const {expiry, keyId} = options
+  const urlObj = new URL(url)
   // Remove existing params to ensure new ones are added in the correct order.
-  baseUrl.searchParams.delete('keyid')
-  baseUrl.searchParams.delete('expiry')
-  baseUrl.searchParams.delete('signature')
+  urlObj.searchParams.delete('keyid')
+  urlObj.searchParams.delete('expiry')
+  urlObj.searchParams.delete('signature')
 
   // Append the signing specific parameters. `set` or `append` after `delete`
   // will have the same effect, but use `append` to be explicit.
-  baseUrl.searchParams.append('keyid', keyId)
+  urlObj.searchParams.append('keyid', keyId)
   const expiryStr = normalizeExpiry(expiry)
   if (expiryStr) {
-    baseUrl.searchParams.append('expiry', expiryStr)
+    urlObj.searchParams.append('expiry', expiryStr)
   }
-  const signature = generateSignature(baseUrl, privateKey)
-  baseUrl.searchParams.append('signature', signature)
 
-  return baseUrl.toString()
+  return urlObj
 }
